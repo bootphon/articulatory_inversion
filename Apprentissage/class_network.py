2f1a3ee6f2ae50e4a5fed876802b5e287803125c
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-06-20 12:00:53 +0200

    kk

--- Apprentissage/class_network.py
+++ Apprentissage/class_network.py
@@ -32,9 +32,9 @@ class my_bilstm(torch.nn.Module):
         self.lstm_layer = torch.nn.LSTM(input_size=hidden_dim,
                                         hidden_size=hidden_dim, num_layers=1,
                                         bidirectional=True)
-        self.lstm_layer_2= torch.nn.LSTM(input_size=hidden_dim*2,
-                                        hidden_size=hidden_dim, num_layers=1,
-                                        bidirectional=True)
+      #  self.lstm_layer_2= torch.nn.LSTM(input_size=hidden_dim*2,
+       #                                 hidden_size=hidden_dim, num_layers=1,
+        #                                bidirectional=True)
         self.readout_layer = torch.nn.Linear(hidden_dim *2, output_dim)
         self.batch_size = batch_size
         self.sigmoid = torch.nn.Sigmoid()
@@ -77,7 +77,7 @@ class my_bilstm(torch.nn.Module):
         dense_out =  torch.nn.functional.relu(self.first_layer(x))
         dense_out_2 = torch.nn.functional.relu(self.second_layer(dense_out))
         lstm_out, hidden_dim = self.lstm_layer(dense_out_2)
-        lstm_out, hidden_dim = self.lstm_layer_2(lstm_out)
+      #  lstm_out, hidden_dim = self.lstm_layer_2(lstm_out)
         lstm_out=torch.nn.functional.relu(lstm_out)
         y_pred = self.readout_layer(lstm_out)
         if self.modele_filtered :
