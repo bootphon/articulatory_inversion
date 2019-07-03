5bac477df5a4b081f340eecb154328a14aabb4dc
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-07-03 14:56:09 +0200

    go back apprendre sur otus

--- Apprentissage/Entrainement_2.py
+++ Apprentissage/Entrainement_2.py
@@ -27,7 +27,7 @@ def train_model(test_on ,n_epochs ,delta_test ,patience ,lr=0.09,to_plot=False):
     data_filtered=True
     modele_filtered=True
     train_on = ["MNGU0", "fsew0", "msak0", "F1", "F5", "M1", "M3", "maps0", "faet0", 'mjjn0', "ffes0"]
-    
+
     train_on.remove(test_on)
     print("train_on :",train_on)
     print("test on:",test_on)
@@ -118,8 +118,8 @@ def train_model(test_on ,n_epochs ,delta_test ,patience ,lr=0.09,to_plot=False):
     N_test = 466
     print('N_train',N_train)
     n_iteration = int(N_train / batch_size)
+    n_iteration = 50
     n_iteration_validation = int(N_valid/batch_size)
-    n_iteration_validation = 50
     n_iteration_test = int(N_test/batch_size)
     patience_temp =0
     test_files_names = []
@@ -150,8 +150,6 @@ def train_model(test_on ,n_epochs ,delta_test ,patience ,lr=0.09,to_plot=False):
 
         #    print("D,E", torch.isnan(model.first_layer.weight.sum()))
             loss = criterion(y,y_pred)
-
-
             loss.backward()
             optimizer.step()
           #  print("ll",x.grad)
