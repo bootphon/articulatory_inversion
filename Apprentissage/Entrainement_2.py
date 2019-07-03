c518bbc2173fffabe4761870b0846fffe22e0880
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-07-03 16:25:57 +0200

    sans mngu0

--- Apprentissage/Entrainement_2.py
+++ Apprentissage/Entrainement_2.py
@@ -27,6 +27,7 @@ def train_model(test_on ,n_epochs ,delta_test ,patience ,lr=0.09,to_plot=False):
     data_filtered=True
     modele_filtered=True
     train_on = ["MNGU0", "fsew0", "msak0", "F1", "F5", "M1", "M3", "maps0", "faet0", 'mjjn0', "ffes0"]
+    train_on = ["fsew0", "msak0", "F1", "F5", "M1", "M3", "maps0", "faet0", 'mjjn0', "ffes0"]
 
     train_on.remove(test_on)
     print("train_on :",train_on)
