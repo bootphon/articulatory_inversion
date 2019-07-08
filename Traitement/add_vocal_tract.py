e5dad1bf2d2f1773194549f75ded1e04d91cf8c8
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-07-08 16:59:11 +0200

    j

--- Traitement/add_vocal_tract.py
+++ Traitement/add_vocal_tract.py
@@ -18,9 +18,11 @@ articulators = [
         'ul_x', 'ul_y', 'll_x', 'll_y']
 
 
+
+
 articulators_after = [
         'tt_x', 'tt_y', 'td_x', 'td_y', 'tb_x', 'tb_y', 'li_x', 'li_y',
-        'ul_x', 'ul_y', 'll_x', 'll_y','la','pro','lar','v_x','v_y'] #détailler
+        'ul_x', 'ul_y', 'll_x', 'll_y','la','pro','ttcl','tbcl','lar','v_x','v_y'] #détailler
 
 root_folder = os.path.dirname(os.getcwd())
 
@@ -35,17 +37,28 @@ def add_vocal_tract(speaker):
         ind_1, ind_2 = [articulators.index("ul_x"), articulators.index("ll_x")]
         lip_protrusion = (ema[:, ind_1] + ema[:, ind_2]) / 2
         return lip_protrusion
-    def add_voicing(wav,ema,sr):
 
+    def add_voicing(wav,ema,sr):
         hop_time = 10 / 1000  # en ms
         hop_length = int((hop_time * sr))
         N_frames = int(len(wav) / hop_length)
         window = scipy.signal.get_window("hanning", N_frames)
         ste = scipy.signal.convolve(wav ** 2, window ** 2, mode="same")
         ste = scipy.signal.resample(ste, num=len(ema))
-        ste = [max(min(x, 1),0) for x in ste]
+        ste = [max(min(x, 1), 0) for x in ste]
 
         return ste
+
+    def add_TTCL(ema): #tongue tip constriction location in degree
+        ind_1, ind_2 = [articulators.index("tt_x"), articulators.index("tt_y")]
+        TTCL = ema[:, ind_1] / np.sqrt(ema[:, ind_1]**2+ema[:,ind_2]**2 ) # upperlip_y - lowerlip_y
+        return TTCL
+
+    def add_TBCL(ema): #tongue body constriction location in degree
+        ind_1, ind_2 = [articulators.index("tb_x"), articulators.index("tb_y")]
+        TBCL = ema[:, ind_1] / np.sqrt(ema[:, ind_1] ** 2 + ema[:, ind_2] ** 2)  # upperlip_y - lowerlip_y
+        return TBCL
+
 
     def add_velum(mfcc,ema):
         if len(mfcc)!= len(ema):
@@ -96,14 +109,14 @@ def add_vocal_tract(speaker):
         ema = np.load(os.path.join(files_path,EMA_files_names[i]+".npy"))
         lip_aperture = add_lip_aperture(ema)
         lip_protrusion = add_lip_protrusion(ema)
-
+        TTCL = add_TTCL(ema)
+        TBCL = add_TBCL(ema)
         if speaker in ["fsew0","msak0","faet0","ffes0"] : # 14 arti de 0 à 13 (2*6 + 2)
             wav,sr = librosa.load(os.path.join(wav_path, EMA_files_names[i] + ".wav"),sr = sampling_rate_wav)
             voicing = add_voicing(wav, ema, sampling_rate_wav)
-            ema = np.insert(ema,(12,13,14),0,axis=1)
-            ema[:,12] = lip_aperture
-            ema[:,13] = lip_protrusion
-            ema[:,14] = voicing
+            velum_xy = ema[:,-2:]
+            ema  = np.concatenate((ema,np.zeros((len(ema),5))),axis=1)
+
 
         elif speaker in ["MNGU0","maps0","mjjn0"]: # 12 arti de 0 à 11
             wav, sr = librosa.load(os.path.join(wav_path, EMA_files_names[i] + ".wav"), sr=sampling_rate_wav)
@@ -111,53 +124,44 @@ def add_vocal_tract(speaker):
             mfcc = np.load(os.path.join(mfcc_path, EMA_files_names[i] + ".npy"))
 
             velum_xy = add_velum(mfcc,ema)
-            ema  = np.concatenate((ema,np.zeros((len(ema),5))),axis=1)
-            ema[:, 12] = lip_aperture
-            ema[:, 13] = lip_protrusion
-            ema[:, 14] = voicing
-
-            ema[:,15:17] = velum_xy
-
+            ema  = np.concatenate((ema,np.zeros((len(ema),7))),axis=1)
 
 
         elif speaker in ["F1","F5","M1","M3"]:
             wav = np.load(os.path.join(wav_path, EMA_files_names[i] + ".npy"))
             mfcc = np.load(os.path.join(mfcc_path,EMA_files_names[i]+".npy"))
-            ema  = np.concatenate((ema,np.zeros((len(ema),5))),axis=1)
+            ema  = np.concatenate((ema,np.zeros((len(ema),7))),axis=1)
 
             if len(ema)!= len(mfcc):
                 print("pbm shape",len(ema),len(mfcc),EMA_files_names[i])
             voicing = add_voicing(wav,ema,sampling_rate_wav)
             velum_xy = add_velum(mfcc,ema)
 
-            ema[:, 12] = lip_aperture
-            ema[:, 13] = lip_protrusion
-            ema[:, 14] = voicing
-            ema[:,15:17] = velum_xy
         elif speaker in  ["F01","F02","F03","F04","M01","M02","M03","M04"] : #haskins
             wav = np.reshape(np.load(os.path.join(wav_path, EMA_files_names[i] + ".npy")),-1)
             mfcc = np.load(os.path.join(mfcc_path, EMA_files_names[i] + ".npy"))
-            ema = np.concatenate((ema, np.zeros((len(ema), 5))), axis=1)
+            ema = np.concatenate((ema, np.zeros((len(ema), 7))), axis=1)
             if len(ema) != len(mfcc):
                 print("pbm shape", len(ema), len(mfcc), EMA_files_names[i])
             voicing = add_voicing(wav, ema, sampling_rate_wav)
             velum_xy = add_velum(mfcc, ema)
-            ema[:, 12] = lip_aperture
-            ema[:, 13] = lip_protrusion
-            ema[:, 14] = voicing
-            ema[:, 15:17] = velum_xy
+
+        ema[:, 12] = lip_aperture
+        ema[:, 13] = lip_protrusion
+        ema[:, 14] = TTCL
+        ema[:, 15] = TBCL
+        ema[:, 16] = voicing
+        ema[:, 17:19] = velum_xy
         np.save(os.path.join(root_folder,"Donnees_pretraitees",speaker_2,"ema_VT",EMA_files_names[i]),ema)
 
 speakers =  ["F01","F02","F03","F04","M01","M02","M03","M04","F5","F1","M1","M3"
     ,"maps0","faet0",'mjjn0',"ffes0","MNGU0","fsew0","msak0"]
 
-# 15
 if __name__=='__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Train and save a model.')
     parser.add_argument('ind', metavar='ind', type=int, help='nombre depochs')
     args = parser.parse_args()
     ind = int(sys.argv[1])
-
     add_vocal_tract(speakers[ind])
 
--- Traitement/split_sentences.py
+++ Traitement/split_sentences.py
@@ -11,31 +11,53 @@ donnees_pretraitees_path = os.path.join(os.path.dirname(os.getcwd()),"Donnees_pr
 speakers = os.listdir(donnees_pretraitees_path)
 speakers.remove("fileset")
 max_length = 500
-def split_in_2(ema,mfcc):
-    to_cut = int(len(mfcc) / 2)
-    mfcc_1 = mfcc[:to_cut]
-    ema_1 = ema[:to_cut, :]
+speakers = speakers[8:]
+print(speakers)
 
 for sp in speakers :
     print("speaker ",sp)
     file_names = os.listdir(os.path.join(donnees_pretraitees_path,sp,"ema"))
+    file_names = [f for f in file_names if 'split' not in f]
+   # file_names = file_names[0:30]
+    Number_cut = 0
     for f in file_names :
         ema = np.load(os.path.join(donnees_pretraitees_path,sp,"ema",f))
         mfcc = np.load(os.path.join(donnees_pretraitees_path,sp,"mfcc",f))
         ema_filtered = np.load(os.path.join(donnees_pretraitees_path,sp,"ema_filtered",f))
         ema_VT = np.load(os.path.join(donnees_pretraitees_path,sp,"ema_VT",f))
 
-        too_long = (len(mfcc)>max_length)
+        cut_in_N = int(len(mfcc)/max_length) +1
+        if cut_in_N > 1 :
+            Number_cut+=1
+            temp = 0
+            cut_size = int(len(mfcc)/cut_in_N)
+            for k in range(cut_in_N-1) :
+                mfcc_k = mfcc[temp : temp + cut_size]
+                ema_k = ema[temp : temp + cut_size,:]
+                ema_k_f = ema_filtered[temp : temp + cut_size,:]
+                ema_k_vt = ema_VT[temp:temp+cut_size,:]
 
-        while too_long :
-            to_cut = int(len(mfcc)/2)
-            mfcc_1 = mfcc[:to_cut]
-            ema_1 = ema[:to_cut,:]
-            ema_fil_1 = ema_filtered[:to_cut,:]
-            ema_vt_1 = ema_VT[:to_cut,:]
-            mfcc_2 = mfcc[to_cut:]
-            ema_2 = ema[to_cut:,:]
+                temp = temp + cut_size
+                np.save(os.path.join(donnees_pretraitees_path,sp,"mfcc",f[:-4]+"_split_"+str(k)),mfcc_k)
+                np.save(os.path.join(donnees_pretraitees_path,sp,"ema",f[:-4]+"_split_"+str(k)),ema_k)
+                np.save(os.path.join(donnees_pretraitees_path,sp,"ema_filtered",f[:-4]+"_split_"+str(k)),ema_k_f)
+                np.save(os.path.join(donnees_pretraitees_path,sp,"ema_VT",f[:-4]+"_split_"+str(k)),ema_k_vt)
 
+            mfcc_last = mfcc[temp :]
+            ema_last = ema[temp : ,:]
+            ema_last_f = ema_filtered[temp: , :]
+            ema_last_vt = ema_VT[temp:, :]
+
+            np.save(os.path.join(donnees_pretraitees_path, sp, "mfcc", f[:-4] + "_split_" + str(cut_in_N-1)), mfcc_last)
+            np.save(os.path.join(donnees_pretraitees_path, sp, "ema", f[:-4] + "_split_" + str(cut_in_N-1)), ema_last)
+            np.save(os.path.join(donnees_pretraitees_path, sp, "ema_filtered", f[:-4] + "_split_" + str(k)), ema_last_f)
+            np.save(os.path.join(donnees_pretraitees_path, sp, "ema_VT", f[:-4] + "_split_" + str(k)), ema_last_vt)
+
+            os.remove(os.path.join(donnees_pretraitees_path,sp,"mfcc",f))
+            os.remove(os.path.join(donnees_pretraitees_path,sp,"ema",f))
+            os.remove(os.path.join(donnees_pretraitees_path,sp,"ema_filtered",f))
+            os.remove(os.path.join(donnees_pretraitees_path,sp,"ema_VT",f))
+    print("number cut for ",sp," : ",Number_cut)
 
 
 
