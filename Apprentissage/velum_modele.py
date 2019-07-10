3d58a0cb89991b86d93f7e9955950a95c984a578
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-07-10 17:46:03 +0200

    import

--- Apprentissage/velum_modele.py
+++ Apprentissage/velum_modele.py
@@ -1,5 +1,8 @@
 import torch
-
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
 from Apprentissage.utils import load_filenames, load_data,load_filenames_deter
 from Apprentissage.pytorchtools import EarlyStopping
 
