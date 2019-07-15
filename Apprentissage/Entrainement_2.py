8be8a74cf968bc44cb8b413a478f664c050101d1
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-07-15 15:31:22 +0200

    kk

--- Apprentissage/Entrainement_2.py
+++ Apprentissage/Entrainement_2.py
@@ -126,8 +126,8 @@ def train_model(test_on ,n_epochs ,delta_test ,patience ,lr=0.09,to_plot=False):
     n_iteration = int(N_train / batch_size)
 
     n_iteration_validation = int(N_valid/batch_size)
-    n_iteration_validation=2
-    n_iteration  =1
+   # n_iteration_validation=2
+  #  n_iteration  =1
     n_iteration_test = int(N_test/batch_size)
     test_files_names = []
 
