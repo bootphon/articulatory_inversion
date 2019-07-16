9042bdf4f42be98fec1c269a74a37aefbd1b6798
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-07-16 14:53:28 +0200

    module

--- Traitement/create_filesets.py
+++ Traitement/create_filesets.py
@@ -1,4 +1,9 @@
 
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
 import numpy as np
 import random
 import os
