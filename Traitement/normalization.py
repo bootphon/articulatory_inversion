95f91d195aea0e4d8e7e12d5c8c7ac98b7260975
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-07-17 14:32:25 +0200

    k

--- Traitement/normalization.py
+++ Traitement/normalization.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 import os
 from os.path import dirname
+from Traitement.fonctions_utiles import get_speakers_per_corpus
 import glob
 def normalize_data(speaker):
     """
@@ -13,20 +14,8 @@ def normalize_data(speaker):
 
     """
   #  print("normalizing for speaker {}".format(speaker))
-    if speaker in ["msak0", "fsew0", "maps0", "faet0", "mjjn0", "ffes0","falh0"]:
-        speaker_2 = "mocha_" + speaker
-
-    elif speaker == "MNGU0":
-        speaker_2 = speaker
-
-    elif speaker in ["F1", "F5", "M1", "M3"]:
-        speaker_2 = "usc_timit_" + speaker
-
-    elif speaker in ["F01", "F02", "F03", "F04", "M01", "M02", "M03", "M04"]:
-        speaker_2 = "Haskins_" + speaker
-
     root_path = dirname(dirname(os.path.realpath(__file__)))
-    path_speaker = os.path.join(root_path,"Donnees_pretraitees",speaker_2)
+    path_speaker = os.path.join(root_path,"Donnees_pretraitees",speaker)
     EMA_files = sorted(
         [name[:-4] for name in os.listdir(os.path.join(path_speaker,"ema_filtered")) if name.endswith(".npy")])
 
@@ -40,35 +29,34 @@ def normalize_data(speaker):
     files += glob.glob(os.path.join(path_speaker, "ema_filtered_norma", "*"))
     for f in files:
         os.remove(f)
-
     N = len(EMA_files)
+    std_ema = np.load(os.path.join(root_path, "Traitement", "norm_values", "std_ema_" + speaker + ".npy"))
+    smoothed_moving_average = np.load(
+        os.path.join(root_path, "Traitement", "norm_values", "moving_average_ema_" + speaker + ".npy"))
+    std_mfcc = np.load(os.path.join(root_path, "Traitement", "norm_values", "std_mfcc_" + speaker + ".npy"))
+    mean_mfcc = np.load(os.path.join(root_path, "Traitement", "norm_values", "mean_mfcc_" + speaker + ".npy"))
 
     for i in range(N):
         ema = np.load(os.path.join(path_speaker, "ema", EMA_files[i] + ".npy"))
         ema_filtered = np.load(os.path.join(path_speaker, "ema_filtered", EMA_files[i] + ".npy"))
-        std_ema= np.load(os.path.join(root_path,"Traitement", "norm_values", "std_ema_" + speaker + ".npy"))
-        smoothed_moving_average = np.load(os.path.join(root_path,"Traitement", "norm_values", "moving_average_ema_" + speaker + ".npy"))
 
         ema = (ema - smoothed_moving_average[i, :])/std_ema
         ema_filtered = (ema_filtered - smoothed_moving_average[i, :]) / std_ema
+
+        mfcc = np.load(os.path.join(path_speaker, "mfcc", EMA_files[i] + ".npy"))
+        mfcc = (mfcc-mean_mfcc)/std_mfcc
+
         np.save(os.path.join(path_speaker, "ema_norma", EMA_files[i]), ema)
         np.save(os.path.join(path_speaker, "ema_filtered_norma", EMA_files[i]), ema_filtered)
+        np.save(os.path.join(path_speaker, "mfcc", EMA_files[i]), mfcc)
+
 
 def normalize_data_per_corpus(corpus) :
-    if corpus == "MNGU0":
-        speakers = ["MNGU0"]
-    elif corpus == "usc":
-        speakers = ["F1", "F5", "M1","M3"]
-    elif corpus == "Haskins":
-        speakers= ["F01","F02","F03","F04","M01","M02","M03","M04"]
-
-    elif corpus == "mocha":
-        speakers =["fsew0","msak0","faet0","ffes0","maps0","mjjn0","falh0"]
-    else :
-        print("vous navez pas choisi un des corpus")
+    speakers = get_speakers_per_corpus(corpus)
     for sp in speakers :
         normalize_data(sp)
-normalize_data("falh0")
+
+#normalize_data("falh0")
 
 #orpus =["Haskins"]
 
