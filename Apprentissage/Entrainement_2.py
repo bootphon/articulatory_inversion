e1be9ff2207514cd335a2377c215c8664cd55825
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-07-18 11:24:14 +0200

    print test a chaque etape

--- Apprentissage/Entrainement_2.py
+++ Apprentissage/Entrainement_2.py
@@ -1,4 +1,8 @@
 ### ETUDIER ARTI 6
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
 
 from class_network import my_bilstm
 from modele import my_ac2art_modele
