268827aea677a158a2712f8e21e0df70ce9ae708
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-07-18 15:33:10 +0200

    main traitement ok

--- Traitement/main_traitement.py
+++ Traitement/main_traitement.py
@@ -1,50 +1,47 @@
-import os,sys,inspect
+import os, sys, inspect
+
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir)
+sys.path.insert(0, parentdir)
 from os.path import dirname
 
-from Traitement.traitement_haskins import traitement_general_haskins
-from Traitement.traitement_mngu0 import traitement_general_mngu0
-from Traitement.traitement_usc_timit_2 import traitement_general_usc_timit
-from Traitement.traitement_mocha import traitement_general_mocha
-from Traitement.add_vocal_tract import add_vocal_tract_per_corpus
-from Traitement.split_sentences import split_sentences
-from Traitement.normalization import normalize_data_per_corpus
+from Traitement.traitement_haskins_new import traitement_general_haskins
+from Traitement.traitement_mngu0_new import traitement_general_mngu0
+from Traitement.traitement_usc_timit_new import traitement_general_usc
+from Traitement.traitement_mocha_new import traitement_general_mocha
 from Traitement.create_filesets import get_fileset_names_per_corpus
 
-def main_traitement(corpus_to_treat = ["mocha","usc","MNGU0","Haskins"],max="All",split=False):
+
+# from Traitement.create_filesets import get_fileset_names_per_corpus
+
+def main_traitement(corpus_to_treat=["mocha", "usc", "MNGU0", "Haskins"], max="All", split=False):
     root_path = dirname(dirname(os.path.realpath(__file__)))
 
-    if not os.path.exists(os.path.join(os.path.join(root_path, "Traitement","norm_values"))):
-            os.makedirs(os.path.join(os.path.join(root_path, "Traitement","norm_values")))
+    if not os.path.exists(os.path.join(os.path.join(root_path, "Traitement", "norm_values"))):
+        os.makedirs(os.path.join(os.path.join(root_path, "Traitement", "norm_values")))
 
-    if "mocha" in corpus_to_treat :
-        traitement_general_mocha(max=max)
+    if "mocha" in corpus_to_treat:
+        traitement_general_mocha(max)
 
     if "MNGU0" in corpus_to_treat:
-        traitement_general_mngu0(max=max)
-        #split = True
+        traitement_general_mngu0(max)
+        # split = True
 
-    if "usc" in corpus_to_treat  :
-        traitement_general_usc_timit(max=max)
+    if "usc" in corpus_to_treat:
+        traitement_general_usc(max)
 
-    if "Haskins" in corpus_to_treat :
-        traitement_general_haskins(max=max)
+    if "Haskins" in corpus_to_treat:
+        traitement_general_haskins(max)
 
-    for corpus in corpus_to_treat :
-        print("norma,addVT for corpus {}".format(corpus))
-        normalize_data_per_corpus(corpus)
-        add_vocal_tract_per_corpus(corpus)
+    for corpus in corpus_to_treat:
         get_fileset_names_per_corpus(corpus)
 
-  #  if split :
-   #     split_sentences()
+if __name__ == '__main__':
+    import argparse
 
+    parser = argparse.ArgumentParser(description='Train and save a model.')
+    parser.add_argument('N_max', metavar='N_max', type=int, help='nombre de fichiers max')
 
-
-
- #   if split :
-  #      split_sentences(corpus=["MNGU0"], max_length=500,max=max)
-
-main_traitement(max="All")
+    args = parser.parse_args()
+    N_max = int(sys.argv[1])
+    main_traitement(max=N_max)
