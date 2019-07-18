156cd590014a48cf3698dbe2049508a82e311ec6
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-07-18 14:38:13 +0200

    kk

--- Apprentissage/Entrainement_2.py
+++ Apprentissage/Entrainement_2.py
@@ -40,7 +40,7 @@ def train_model(test_on ,n_epochs ,delta_test ,patience ,lr=0.09,to_plot=False):
                  "M3","maps0","faet0",'mjjn0',"ffes0","MNGU0","fsew0","msak0"]
 
 
-
+    train_on = ["F1","F5","M1","M3"]
     train_on.remove(test_on)
     print("train_on :",train_on)
     print("test on:",test_on)
