f912f29c22334939d7c7379f42d3501276020d63
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-07-24 16:44:35 +0200

    change ema_final

--- Traitement/split_sentences.py
+++ Traitement/split_sentences.py
@@ -11,8 +11,7 @@ root_folder = os.path.dirname(os.getcwd())
 # file_names = file_names[0:30]
 def split_sentences(speaker="MNGU0" ,max_length = 300,N="All"):
     donnees_pretraitees_path = os.path.join(root_folder, "Donnees_pretraitees")
-
-    file_names = os.listdir(os.path.join(donnees_pretraitees_path, speaker, "ema_VT"))
+    file_names = os.listdir(os.path.join(donnees_pretraitees_path, speaker, "ema_final"))
     file_names = [f for f in file_names if 'split' not in f]
     if N == "All":
         N = len(file_names)
@@ -23,7 +22,7 @@ def split_sentences(speaker="MNGU0" ,max_length = 300,N="All"):
        # ema = np.load(os.path.join(donnees_pretraitees_path,sp,"ema",f))
         mfcc = np.load(os.path.join(donnees_pretraitees_path,speaker,"mfcc",f))
         #ema_filtered = np.load(os.path.join(donnees_pretraitees_path,sp,"ema_filtered",f))
-        ema_VT = np.load(os.path.join(donnees_pretraitees_path,speaker,"ema_VT",f))
+        ema_VT = np.load(os.path.join(donnees_pretraitees_path,speaker,"ema_final",f))
         cut_in_N = int(len(mfcc)/max_length) +1
         if cut_in_N > 1 :
          #   print("cur in ",cut_in_N)
@@ -41,7 +40,7 @@ def split_sentences(speaker="MNGU0" ,max_length = 300,N="All"):
                 np.save(os.path.join(donnees_pretraitees_path,speaker,"mfcc",f[:-4]+"_split_"+str(k)),mfcc_k)
              #   np.save(os.path.join(donnees_pretraitees_path,sp,"ema",f[:-4]+"_split_"+str(k)),ema_k)
              #   np.save(os.path.join(donnees_pretraitees_path,sp,"ema_filtered",f[:-4]+"_split_"+str(k)),ema_k_f)
-                np.save(os.path.join(donnees_pretraitees_path,speaker,"ema_VT",f[:-4]+"_split_"+str(k)),ema_k_vt)
+                np.save(os.path.join(donnees_pretraitees_path,speaker,"ema_final",f[:-4]+"_split_"+str(k)),ema_k_vt)
 
 
             mfcc_last = mfcc[temp :]
@@ -51,12 +50,12 @@ def split_sentences(speaker="MNGU0" ,max_length = 300,N="All"):
             np.save(os.path.join(donnees_pretraitees_path, speaker, "mfcc", f[:-4] + "_split_" + str(cut_in_N-1)), mfcc_last)
             #np.save(os.path.join(donnees_pretraitees_path, sp, "ema", f[:-4] + "_split_" + str(cut_in_N-1)), ema_last)
             #np.save(os.path.join(donnees_pretraitees_path, sp, "ema_filtered", f[:-4] + "_split_" + str(k)), ema_last_f)
-            np.save(os.path.join(donnees_pretraitees_path, speaker, "ema_VT", f[:-4] + "_split_" + str(cut_in_N-1)), ema_last_vt)
+            np.save(os.path.join(donnees_pretraitees_path, speaker, "ema_final", f[:-4] + "_split_" + str(cut_in_N-1)), ema_last_vt)
 
             os.remove(os.path.join(donnees_pretraitees_path,speaker,"mfcc",f))
             #os.remove(os.path.join(donnees_pretraitees_path,sp,"ema",f))
             #os.remove(os.path.join(donnees_pretraitees_path,sp,"ema_filtered",f))
-            os.remove(os.path.join(donnees_pretraitees_path,speaker,"ema_VT",f))
+            os.remove(os.path.join(donnees_pretraitees_path,speaker,"ema_final",f))
    # print("number cut for ",speaker," : ",Number_cut)
 
 
