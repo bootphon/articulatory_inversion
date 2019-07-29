6e8b14ffc94abfc244073366ddcc2e3449e315e3
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-07-29 11:25:05 +0200

    err

--- Apprentissage/modele.py
+++ Apprentissage/modele.py
@@ -242,7 +242,7 @@ class my_ac2art_modele(torch.nn.Module):
         rmse_per_arti_mean = np.mean(all_diff, axis=0)
 
         #pearson_per_arti_std = np.std(all_pearson, axis=0)
-        print("rmse final : ", np.mean(all_diff[pearson_per_arti_mean != 0]))
+        print("rmse final : ", np.mean(rmse_per_arti_mean[pearson_per_arti_mean != 0]))
         print("rmse mean per arti : \n", rmse_per_arti_mean)
         print("pearson final : ", np.mean(pearson_per_arti_mean[pearson_per_arti_mean!=0]))
         print("pearson mean per arti : \n", pearson_per_arti_mean)
