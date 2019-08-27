d9e7ef5ebb0116713642e95257a7d623806f4aa4
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-08-27 15:32:53 +0200

    temp filter 0

--- Apprentissage/experiences_cross_val_haskins.py
+++ Apprentissage/experiences_cross_val_haskins.py
@@ -18,7 +18,7 @@ def cross_val_for_type_filter_has(speaker): #0 1 ou 2
     corpus_to_train_on = str(corpus)
     batch_norma = False
     loss_train = "both_90"
-    for filter_type in [1,2]:
+    for filter_type in [0] :# [0,1,2]:
         train_model(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train, patience=patience,
                     select_arti=select_arti, corpus_to_train_on=corpus_to_train_on,
                     batch_norma=batch_norma, filter_type=filter_type)
