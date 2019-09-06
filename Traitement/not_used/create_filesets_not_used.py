98c99f5693cca91dcc0fd9f34b0d1fa968c57717
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-09-06 12:20:20 +0200

    not used

---
+++ Traitement/not_used/create_filesets_not_used.py
@@ -0,0 +1,105 @@
+
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
+import numpy as np
+import random
+import os
+from os.path import dirname
+from random import shuffle
+from Traitement.fonctions_utiles import get_speakers_per_corpus
+import csv
+import json
+
+
+
+root_folder = os.path.dirname(os.getcwd())
+donnees_path = os.path.join(root_folder, "Donnees_pretraitees")
+
+def get_fileset_names(speaker):
+    """
+    :param speaker: un des speaker
+    :return: rien
+    Ecrit pour le speaker 3 fichiers txt sp_train, sp_test, sp_valid avec les noms des fichiers du train/test/validation set
+    """
+
+    files_path =  os.path.join(donnees_path,speaker)
+    EMA_files_names = [name[:-4] for name in os.listdir(os.path.join(files_path,"ema_final")) if name.endswith('.npy') ]
+    N = len(EMA_files_names)
+    shuffle(EMA_files_names)
+    pourcent_train = 0.7
+    pourcent_test=0.2
+    n_train = int(N*pourcent_train)
+    n_test  = int(N*pourcent_test)
+    train_files = EMA_files_names[:n_train]
+    test_files = EMA_files_names[n_train:n_train+n_test]
+    valid_files = EMA_files_names[n_train+n_test:]
+
+    outF = open(os.path.join(root_folder,"Donnees_pretraitees","fileset",speaker+"_train.txt"), "w")
+    outF.write('\n'.join(train_files) + '\n')
+    outF.close()
+
+    outF = open(os.path.join(root_folder, "Donnees_pretraitees", "fileset", speaker + "_test.txt"), "w")
+    outF.write('\n'.join(test_files) + '\n')
+    outF.close()
+
+    outF = open(os.path.join(root_folder, "Donnees_pretraitees", "fileset", speaker + "_valid.txt"), "w")
+    outF.write('\n'.join(valid_files) + '\n')
+    outF.close()
+
+
+
+def get_fileset_names_per_corpus(corpus):
+    """
+    :param corpus: un des corpus "mocha","usc","MNGU0","Haskins"
+    :return:  rien, crée les fileset pour tous les speaker du corpus
+    """
+    speakers = get_speakers_per_corpus(corpus)
+    for sp in speakers :
+        try:
+            get_fileset_names(sp)
+        except :
+            print("Pbm pour creer le fileset de sp ,",sp)
+
+
+#get_fileset_names_per_corpus("MNGU0")
+def read_csv_arti_ok_per_speaker():
+    """
+    :return:
+    dictionnaire avec en clé les différentes categories de speaker (categ de A à F pour le moment). Au sein
+    d'une catégorie les speakers ont les mêmes arti valides. Ces catégories sont tirées du fichier CSV qui est lu
+    et peut être modifié par l'utilisateur.
+    La valeur associée à une categorie est un autre dictionnaire donnant les speakers concernés par cette catégorie
+    et les articulateurs concernés, sous forme d'une liste de 18 0 et 1, avec un 1 pour les arti valides.
+    """
+    arti_per_speaker = os.path.join(root_folder,"Traitement", "articulators_per_speaker.csv")
+    csv.register_dialect('myDialect', delimiter=';')
+    categ_of_speakers = dict()
+    with open(arti_per_speaker, 'r') as csvFile:
+        reader = csv.reader(csvFile, dialect="myDialect")
+        next(reader)
+        for categ in ["A", "B", "C", "D", "E", "F"]:
+            categ_of_speakers[categ] = dict()
+            categ_of_speakers[categ]["sp"] = []
+            categ_of_speakers[categ]["arti"] = None
+        for row in reader:
+            categ_of_speakers[row[19]]["sp"].append(row[0])
+            if categ_of_speakers[row[19]]["arti"]  :
+                if categ_of_speakers[row[19]]["arti"] != row[1:19]:
+                    print("check arti and category for categ {}".format(row[19]))
+            else:
+                categ_of_speakers[row[19]]["arti"] = row[1:19]
+
+    for cle in categ_of_speakers.keys():
+        print("categ ",cle)
+        print(categ_of_speakers[cle])
+
+    with open(os.path.join(root_folder,"Apprentissage","categ_of_speakers.json"), 'w') as dico:
+        json.dump(categ_of_speakers, dico)
+
+#read_csv_arti_ok_per_speaker()
+
+
+
---
+++ Traitement/not_used/split_sentences_notused.py
@@ -0,0 +1,78 @@
+
+import numpy as np
+import random
+import os
+from os.path import dirname
+from Traitement.fonctions_utiles import get_speakers_per_corpus
+root_folder = os.path.dirname(os.getcwd())
+
+
+
+# file_names = file_names[0:30]
+def split_sentences(speaker="MNGU0" ,max_length = 300,N="All"):
+    """
+
+    :param speaker:
+    :param max_length: nombre de points max qu'on veut par phrase (durée = max_lenghts*100 secondes)
+    :param N: ne s'occupe que des N premières phrases
+    :return: rien.
+    parcourt les fichiers dans "mfcc" et "ema_final". Si leur longueur est > max_length, alors il est divisé en K
+    de telle sorte que chaque bout soit de longueur < max_length.
+    - Attention les fichiers non splittées sont alors supprimées, et à la place K fichiers dans mfcc et dans ema_final.
+    Si la phrase 80 est trop grande et doit être coupée en deux on n'aura plus les fichiers "mfcc_80" ni "ema_80" mais à
+    la place on aura "mfcc_80_split_0", "mfcc_80_split_1" "ema_80_split_0" et "ema_80_split_1".
+    - Attention les fichiers ema ne sont splittés que dans ema_final (ce sont ceux qui sont à terme utilsés pour l'apprentissage).
+    """
+    donnees_pretraitees_path = os.path.join(root_folder, "Donnees_pretraitees")
+    file_names = os.listdir(os.path.join(donnees_pretraitees_path, speaker, "ema_final"))
+    file_names = [f for f in file_names if 'split' not in f]
+    if N == "All":
+        N = len(file_names)
+    file_names = file_names[0:N]
+
+    Number_cut = 0
+    for f in file_names :
+       # ema = np.load(os.path.join(donnees_pretraitees_path,sp,"ema",f))
+        mfcc = np.load(os.path.join(donnees_pretraitees_path,speaker,"mfcc",f))
+        #ema_filtered = np.load(os.path.join(donnees_pretraitees_path,sp,"ema_filtered",f))
+        ema_VT = np.load(os.path.join(donnees_pretraitees_path,speaker,"ema_final",f))
+        cut_in_N = int(len(mfcc)/max_length) +1
+        if cut_in_N > 1 :
+         #   print("cur in ",cut_in_N)
+            Number_cut+=1
+            temp = 0
+            cut_size = int(len(mfcc)/cut_in_N)
+            for k in range(cut_in_N-1) : # si k va jusqua 0 ca veut cut_in_N-1 vaut 1 cut_in_N vaut 2
+                #print("k ,",k)
+                mfcc_k = mfcc[temp : temp + cut_size]
+        #        ema_k = ema[temp : temp + cut_size,:]
+         #       ema_k_f = ema_filtered[temp : temp + cut_size,:]
+                ema_k_vt = ema_VT[temp:temp+cut_size,:]
+
+                temp = temp + cut_size
+                np.save(os.path.join(donnees_pretraitees_path,speaker,"mfcc",f[:-4]+"_split_"+str(k)),mfcc_k)
+             #   np.save(os.path.join(donnees_pretraitees_path,sp,"ema",f[:-4]+"_split_"+str(k)),ema_k)
+             #   np.save(os.path.join(donnees_pretraitees_path,sp,"ema_filtered",f[:-4]+"_split_"+str(k)),ema_k_f)
+                np.save(os.path.join(donnees_pretraitees_path,speaker,"ema_final",f[:-4]+"_split_"+str(k)),ema_k_vt)
+
+            mfcc_last = mfcc[temp :]
+          #  ema_last = ema[temp : ,:]
+           # ema_last_f = ema_filtered[temp: , :]
+            ema_last_vt = ema_VT[temp:, :]
+            np.save(os.path.join(donnees_pretraitees_path, speaker, "mfcc", f[:-4] + "_split_" + str(cut_in_N-1)), mfcc_last)
+            #np.save(os.path.join(donnees_pretraitees_path, sp, "ema", f[:-4] + "_split_" + str(cut_in_N-1)), ema_last)
+            #np.save(os.path.join(donnees_pretraitees_path, sp, "ema_filtered", f[:-4] + "_split_" + str(k)), ema_last_f)
+            np.save(os.path.join(donnees_pretraitees_path, speaker, "ema_final", f[:-4] + "_split_" + str(cut_in_N-1)), ema_last_vt)
+
+            os.remove(os.path.join(donnees_pretraitees_path,speaker,"mfcc",f))
+            #os.remove(os.path.join(donnees_pretraitees_path,sp,"ema",f))
+            #os.remove(os.path.join(donnees_pretraitees_path,sp,"ema_filtered",f))
+            os.remove(os.path.join(donnees_pretraitees_path,speaker,"ema_final",f))
+   # print("number cut for ",speaker," : ",Number_cut)
+
+#split_sentences("MNGU0")
+
+#speakers = get_speakers_per_corpus("usc")
+#for sp in speakers:
+#    split_sentences(speaker = sp)
+
