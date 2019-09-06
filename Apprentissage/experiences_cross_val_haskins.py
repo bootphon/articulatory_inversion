fe36f88b5b87c22399c940042c3d0fa3f5b4b7f1
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-09-06 11:55:17 +0200

    pas mal de changement

--- Apprentissage/experiences_cross_val_haskins.py
+++ Apprentissage/experiences_cross_val_haskins.py
@@ -8,79 +8,131 @@ from Apprentissage.train import train_model
 from Traitement.fonctions_utiles import get_speakers_per_corpus
 import sys
 from Apprentissage.train_on_speaker import train_model_on_speaker
+from Apprentissage.train_all_param import train_model_new
+import numpy as np
 from multiprocessing import Process
 import argparse
 corpus = "[Haskins]"
 speakers = get_speakers_per_corpus("Haskins")
 
 
+# CREER UN EXCEL ET Y METTRE LES RESULTATS DES EXPERIENCES, 1 EXCEL (ou csv) PAR EXPERIENCE
+# PREMIERE LIGNE EXPLICATION DE LEXPERIENCE ET DES PARAMETRES FIXES
+# PUIS PREMIERE COLONNE CE QUI VARIE ET LA SUITE LES RESULTATS
 
-def cross_val_for_type_filter_has(speaker): #influence du filtre ==> pas bcp
-    patience = 3
-    n_epochs = 50
-    select_arti = True
+# plus souvent fixe
+n_epochs = 50
+loss_train = "both_90"
+patience = 5
+select_arti = True
+batch_norma = False
+filter_type = 1
+to_plot = False
+lr = 0.001
+delta_test = 1
+corpus = ["Haskins"]
+train_a_bit_on_test = False
+output_dim = 18 #18 trajectories
+speakers = get_speakers_per_corpus(corpus)
+
+
+
+def cross_val_for_type_filter_has_indep(): #influence du filtre ==> pas bcp
     corpus_to_train_on = str(corpus)
-    batch_norma = False
-    loss_train = "both_90"
-    for filter_type in [0] :# [0,1,2]:
-        train_model(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train, patience=patience,
-                    select_arti=select_arti, corpus_to_train_on=corpus_to_train_on,
-                    batch_norma=batch_norma, filter_type=filter_type,train_a_bit_on_test=False)
+    for filter_type in [0,1,2] :
+        count = 0
+        rmse_all, pearson_all = np.zeros((len(speakers), output_dim)), np.zeros((len(speakers), output_dim))
+        for speaker in speakers :
+            rmse,pearson  = train_model_new(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train, patience=patience,
+                        select_arti=select_arti, corpus_to_train_on=corpus_to_train_on, batch_norma=batch_norma,
+                        filter_type=filter_type, train_a_bit_on_test=train_a_bit_on_test, to_plot=to_plot
+                        , lr=lr, delta_test=delta_test)
+            rmse_all[count,:] = rmse
+            pearson_all[count,:]= pearson
+            count += 1
+        results_rmse = np.mean(rmse_all,axis=0)
+        results_pearson = np.mean(pearson_all,axis = 0)
+        std_rmse = np.std(rmse_all, axis = 0 )
+        std_pearson  = np.std(pearson_all, axis = 0 )
+        print("for filter type {} results are".format(filter_type))
+        print("RMSE mean ", results_rmse) #aulieu de plotter mettre dans un csv ?
+        print("RMSE std ", std_rmse)
+        print("PEARSON ", results_pearson)
+        print(" PEARSON STD")
 
 
-def cross_val_for_alpha_has(speaker): #influence de alpha ==> a voir..
-    patience = 3
-    n_epochs = 50
-    select_arti = True
-    corpus_to_train_on = corpus
-    filter_type = 1
-    batch_norma = False
-    for alpha in [0,40,100] :
+def cross_val_for_bn_has_indep(): #influence du filtre ==> pas bcp
+    corpus_to_train_on = str(corpus)
+    for batch_norma in ["True","False"] :
+        count = 0
+        rmse_all, pearson_all = np.zeros((len(speakers), output_dim)), np.zeros((len(speakers), output_dim))
+
+        for speaker in speakers :
+            rmse,pearson  = train_model_new(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train, patience=patience,
+                        select_arti=select_arti, corpus_to_train_on=corpus_to_train_on, batch_norma=batch_norma,
+                        filter_type=filter_type, train_a_bit_on_test=train_a_bit_on_test, to_plot=to_plot
+                        , lr=lr, delta_test=delta_test)
+            rmse_all[count,:] = rmse
+            pearson_all[count,:]= pearson
+            count += 1
+
+        results_rmse = np.mean(rmse_all,axis=0)
+        results_pearson = np.mean(pearson_all,axis = 0)
+        print("for bn {} results are".format(batch_norma))
+        print("RMSE ", results_rmse) #aulieu de plotter mettre dans un csv ?
+        print("PEARSON ", results_pearson)
+
+
+
+
+def cross_val_for_alpha_has_indep(): #influence du filtre ==> pas bcp
+    corpus_to_train_on = str(corpus)
+    for alpha in [0,20,40,60,80,100] :
+        count = 0
         loss_train = "both_" + str(alpha)
-        train_model(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train, patience=patience,
-                    select_arti=select_arti, corpus_to_train_on=corpus_to_train_on,
-                    batch_norma=batch_norma, filter_type=filter_type,train_a_bit_on_test=False)
+        rmse_all, pearson_all = np.zeros((len(speakers), output_dim)), np.zeros((len(speakers), output_dim))
+        for speaker in speakers :
+            rmse,pearson  = train_model_new(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train, patience=patience,
+                        select_arti=select_arti, corpus_to_train_on=corpus_to_train_on, batch_norma=batch_norma,
+                        filter_type=filter_type, train_a_bit_on_test=train_a_bit_on_test, to_plot=to_plot
+                        , lr=lr, delta_test=delta_test)
+            rmse_all[count,:] = rmse
+            pearson_all[count,:]= pearson
+            count += 1
+
+        results_rmse = np.mean(rmse_all,axis=0)
+        results_pearson = np.mean(pearson_all,axis = 0)
+        print("for alpha {} results are".format(alpha))
+        print("RMSE ", results_rmse) #aulieu de plotter mettre dans un csv ?
+        print("PEARSON ", results_pearson)
 
 
-def cross_val_for_bn_has(speaker):  # influence de bn bof
-
-    patience = 3
-    n_epochs = 50
-    select_arti = True
-    corpus_to_train_on = corpus
-    filter_type = 1
-    loss_train = "both_90"
-    train_model(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train, patience=patience,
-                select_arti=select_arti, corpus_to_train_on=corpus_to_train_on,
-                batch_norma=False, filter_type=filter_type, train_a_bit_on_test=False)
-
-    train_model(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train, patience=patience,
-                select_arti=select_arti, corpus_to_train_on=corpus_to_train_on,
-                batch_norma=True, filter_type=filter_type, train_a_bit_on_test=False)
 
 
-def cross_val_for_rmse_has(speaker): #resultats finaux loss rmse generalisation
-    patience = 3
-    n_epochs = 50
-    select_arti = True
-    corpus_to_train_on = corpus
-    filter_type = 1
-    loss_train ="rmse"
-    train_model(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train, patience=patience,
-                    select_arti=select_arti, corpus_to_train_on=corpus_to_train_on,
-                    batch_norma=False, filter_type=filter_type,train_a_bit_on_test=False)
+def cross_val_indep(): #influence du filtre ==> pas bcp
+    corpus_to_train_on = str(corpus)
+    for alpha in [0,20,40,60,80,100] :
+        count = 0
+        loss_train = "both_" + str(alpha)
+        rmse_all, pearson_all = np.zeros((len(speakers), output_dim)), np.zeros((len(speakers), output_dim))
+        for speaker in speakers :
+            rmse,pearson  = train_model_new(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train, patience=patience,
+                        select_arti=select_arti, corpus_to_train_on=corpus_to_train_on, batch_norma=batch_norma,
+                        filter_type=filter_type, train_a_bit_on_test=train_a_bit_on_test, to_plot=to_plot
+                        , lr=lr, delta_test=delta_test)
+            rmse_all[count,:] = rmse
+            pearson_all[count,:]= pearson
+            count += 1
+
+        results_rmse = np.mean(rmse_all,axis=0)
+        results_pearson = np.mean(pearson_all,axis = 0)
+        print("for alpha {} results are".format(alpha))
+        print("RMSE ", results_rmse) #aulieu de plotter mettre dans un csv ?
+        print("PEARSON ", results_pearson)
 
 
-def cross_val_for_rmse_has_and_test_speaker(speaker): #resultats de non généralisation rmse
-    patience = 3
-    n_epochs = 50
-    select_arti = True
-    corpus_to_train_on = corpus
-    filter_type = 1
-    loss_train ="rmse"
-    train_model(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train, patience=patience,
-                    select_arti=select_arti, corpus_to_train_on=corpus_to_train_on,
-                    batch_norma=False, filter_type=filter_type,train_a_bit_on_test=True)
+
+
 
 def cross_val_for_both_90_has_and_test_speaker(speaker): #resultats de non généralisation both90
     patience = 3
@@ -96,9 +148,6 @@ def cross_val_for_both_90_has_and_test_speaker(speaker): #resultats de non gén�
 def speaker_dependant(speaker):
     train_model_on_speaker(speaker,"rmse","non")
     train_model_on_speaker(speaker,"both_90","non")
-
-
-
 
 if __name__=='__main__':
     experience = sys.argv[1]
