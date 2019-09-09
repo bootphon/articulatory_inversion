4146a2d7768a3633a1fab94f0a559aaa203e2bb1
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-09-09 12:09:08 +0200

    doc

--- Traitement/main_traitement.py
+++ Traitement/main_traitement.py
@@ -9,35 +9,17 @@ from Traitement.traitement_haskins import traitement_general_haskins
 from Traitement.traitement_mngu0 import traitement_general_mngu0
 from Traitement.traitement_usc_timit import traitement_general_usc
 from Traitement.traitement_mocha import traitement_general_mocha
-from Traitement.create_filesets import get_fileset_names_per_corpus
 import argparse
 from multiprocessing import Process
-import time
-# from Traitement.create_filesets import get_fileset_names_per_corpus
-
-def main_traitement(corpus_to_treat=["mocha", "usc", "MNGU0", "Haskins"], max="All", split=False):
-    # TODO
-    root_path = dirname(dirname(os.path.realpath(__file__)))
-
-    if not os.path.exists(os.path.join(os.path.join(root_path, "Traitement", "norm_values"))):
-        os.makedirs(os.path.join(os.path.join(root_path, "Traitement", "norm_values")))
-
-    if "mocha" in corpus_to_treat:
-        traitement_general_mocha(max)
-
-    if "MNGU0" in corpus_to_treat:
-        traitement_general_mngu0(max)
-        # split = True
-
-    if "usc" in corpus_to_treat:
-        traitement_general_usc(max)
-
-    if "Haskins" in corpus_to_treat:
-        traitement_general_haskins(max)
 
 
 def traitement_general_per_corpus(corp,max):
-    # TODO
+    """
+    :param corp: corpus we want to do the preprocess
+    :param max:  max of files to preprocess (useful for test), 0 to treat all files
+    perform the preprocess for the asked corpus
+     """
+
     if corp == "MNGU0":
         traitement_general_mngu0(max)
     elif corp == "usc":
@@ -47,24 +29,20 @@ def traitement_general_per_corpus(corp,max):
     elif corp == "mocha":
         traitement_general_mocha(max)
 
-#if __name__ == '__main__':
-    #import argparse
-
-   # parser = argparse.ArgumentParser(description='Train and save a model.')
-   # parser.add_argument('N_max', metavar='N_max', type=int, help='nombre de fichiers max')
-
-   # args = parser.parse_args()
-  #  N_max = int(sys.argv[1])
- #   main_traitement(max=N_max)
 
 if __name__ == '__main__':
-    #TODO : ok c'est super ça, il faut juste que tu expliques dans le readme comment tu utilsies ça
+    """
+    from the cmd to launch preprocess for all the corpuses,
+    parallel computing, 1 processor per corpus 
+    """
     corpus = ["MNGU0","mocha","usc","Haskins"]
     procs = []
 
-    parser = argparse.ArgumentParser(description='Train and save a model.')
+    parser = argparse.ArgumentParser(description='preprocessing of all the corpuses with parallelization')
     parser.add_argument('N_max', metavar='N_max', type=int,
-                        help='nombre de fichiers max que lon veut traiter par corpus')
+                        help='Nmax we want to preprocess, 0 for all')
+
+    #TO MAKE N_MAX DEFAULT AT 0 ...parser.add_argument('--n_max', metavar='N_max', type=int,help='Nmax we want to preprocess, 0 for all' , default=0)
 
     args = parser.parse_args()
     N_max = int(sys.argv[1])
@@ -75,3 +53,29 @@ if __name__ == '__main__':
 
     for proc in procs:
         proc.join()
+
+
+
+#not used anymore
+def main_traitement(corpus_to_treat=["mocha", "usc", "MNGU0", "Haskins"], max=0):
+    """
+    :param corpus_to_treat: corpus we want to do the preprocess
+    :param max:  # max of files to preprocess (useful for test), if 0 all files
+    to preprocessing of each corpus one after the other calling the "traitement_general_&corpus" fonction
+    """
+    root_path = dirname(dirname(os.path.realpath(__file__)))
+
+    if not os.path.exists(os.path.join(os.path.join(root_path, "Traitement", "norm_values"))):
+        os.makedirs(os.path.join(os.path.join(root_path, "Traitement", "norm_values")))
+
+    if "mocha" in corpus_to_treat:
+        traitement_general_mocha(max)
+
+    if "MNGU0" in corpus_to_treat:
+        traitement_general_mngu0(max)
+
+    if "usc" in corpus_to_treat:
+        traitement_general_usc(max)
+
+    if "Haskins" in corpus_to_treat:
+        traitement_general_haskins(max)
