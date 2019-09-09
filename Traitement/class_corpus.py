467f71aa4337484a796fe3bbaf4a047056220063
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-09-09 10:49:17 +0200

    quelques commentaires et changements

--- Traitement/class_corpus.py
+++ Traitement/class_corpus.py
@@ -1,33 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    Created 29 august 2019
+    Created august 2019
     by Maud Parrot
-    TODO : mettre cette entête sur toutes tes pages de code, avec un dexcriptif de ce que ça fait (j'ai mis une date random, tu peux mettre 'summer 2019)
+    2 classes useful since we have 4 corpuses and 19 speakers. each corpus has common attributes, and there are
+    common attributes for all speakers as well (and function for the preprocessing)
+    1 class for the corpus instance ,  contains parameters about the corpus
+    1 class for the speaker instance, inherit of corpus class,  contains functions for the preprocessing
 """
 import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
-# TODO: beaucoup d'import on l'air inutiles, enlève les si c'ets le cas
 import os
-import time
-from os.path import dirname
 import numpy as np
 import scipy.signal
-from scipy import stats
-import matplotlib.pyplot as plt
 import scipy.interpolate
-import librosa
-from scipy.fftpack import fft, ifft
 from Traitement.fonctions_utiles import low_pass_filter_weight
-import shutil
-
-from Traitement.create_filesets import get_fileset_names
-import glob
 import csv
-import multiprocessing as mp
 
 
 
@@ -36,44 +27,54 @@ root_folder = os.path.dirname(os.getcwd())
 
 
 class Corpus():
-    # TODO description de la classe et son utilité
-    def __init__(self,name):
-        # TODO Faire un descriptif de chaque fonction sous cette forme:
+    """
+    There are some common attributes among all corpuses , this class avoid redefining those attributes
+    in each preprocessing script.
+    Parent of the speaker class
+    attention never used , only speaker is used in the new version of preprocessing
+    """
+    def __init__(self, name):
         """
-        Explication de la fonction
-        :param name: dire ce que c'est et type
-        (si la fonction return quelque chose, tu ajoutes;
-        return: description de ce que c'est et type)
+        :param name: name of the corpus (Haskins , MNGU0, USC or mocha)
         """
         super(Corpus, self).__init__()
         self.name = name
+        self.sampling_rate_wav_wanted = 16000
+        self.frame_time = 25/1000
+        self.hop_time = 10/1000
+        self.hop_length = int(self.hop_time * self.sampling_rate_wav_wanted)
+        self.frame_length = int(self.frame_time * self.sampling_rate_wav_wanted)
+        self.window = 5
+        self.n_coeff = 13
         self.sampling_rate_ema = None
         self.sampling_rate_wav = None
         self.speakers = None
         self.get_speakers()
-        self.articulators = [ 'tt_x', 'tt_y', 'td_x', 'td_y', 'tb_x', 'tb_y', 'li_x', 'li_y',
-        'ul_x', 'ul_y', 'll_x', 'll_y']
+        self.articulators = ['tt_x', 'tt_y', 'td_x', 'td_y', 'tb_x', 'tb_y', 'li_x', 'li_y'
+                             , 'ul_x', 'ul_y', 'll_x', 'll_y']
         self.speakers_with_velum = ["fsew0", "msak0", "faet0", "ffes0", "falh0"]
-
         self.init_variables()
 
     def get_speakers(self):
-        #TODO
+        """
+        define the list of speakers for this corpus
+        """
         if self.name == "MNGU0":
             speakers = ["MNGU0"]
         elif self.name == "usc":
             speakers = ["F1", "F5", "M1", "M3"]
-        elif self.name ==  "Haskins":
+        elif self.name == "Haskins":
             speakers = ["F01", "F02", "F03", "F04", "M01", "M02", "M03", "M04"]
         elif self.name == "mocha":
             speakers = ["fsew0", "msak0", "faet0", "ffes0", "maps0", "mjjn0", "falh0"]
         else:
             raise NameError("vous navez pas choisi un des corpus")
-        self.speakers =  speakers
-
+        self.speakers = speakers
 
     def init_variables(self):  #créer un script qui lit un fichier csv avec ces données
-        #TODO
+        """
+        Initialize some parameters depending on the corpus
+        """
         if self.name == "mocha":
             self.sampling_rate_wav = 16000
             self.sampling_rate_ema = 500
@@ -95,24 +96,23 @@ class Corpus():
             self.cutoff = 20
 
 
-class Speaker():
-    #TODO
-    def __init__(self,name):
-        #TODO
-        self.name = name
+class Speaker(Corpus):
+    """
+    The speakers share some preprocessing function.
+    They have some specific attributes that are defined by the parent class (Corpus)
+    This class is used in each preprocessing script
+    """
+    def __init__(self,speaker):
+        """
+        :param name:  name of the speaker
+        """
+        self.speaker = speaker
         self.speakers = None
-        self.corpus_name = None
+        self.corpus = None
         self.get_corpus_name()
-        self.corpus = Corpus(self.corpus_name)
-
+        super().__init__(self.corpus)
         self.EMA_files = None
-        self.sampling_rate_wav  =self.corpus.sampling_rate_wav
-        self.sampling_rate_ema = self.corpus.sampling_rate_ema
-        self.cutoff  = self.corpus.cutoff
-        self.articulators = self.corpus.articulators
-        self.speakers_with_velum = self.corpus.speakers_with_velum
-
-        if self.name in self.speakers_with_velum:
+        if self.speaker in self.speakers_with_velum:
             self.articulators = ['tt_x', 'tt_y', 'td_x', 'td_y', 'tb_x', 'tb_y', 'li_x', 'li_y',
                                  'ul_x', 'ul_y', 'll_x', 'll_y', 'v_x', 'v_y']
 
@@ -125,24 +125,28 @@ class Speaker():
         self.std_mfcc = None
         self.mean_mfcc = None
 
-
-
     def get_corpus_name(self):
-        #TODO
-        if self.name == "MNGU0":
+        """
+        define the corpus the speaker comes from
+        """
+        if self.speaker == "MNGU0":
             corpus = "MNGU0"
-        elif self.name in ["F1", "F5", "M1", "M3"]:
+        elif self.speaker in ["F1", "F5", "M1", "M3"]:
             corpus ="usc"
-        elif self.name in   ["F01", "F02", "F03", "F04", "M01", "M02", "M03", "M04"] :
+        elif self.speaker in   ["F01", "F02", "F03", "F04", "M01", "M02", "M03", "M04"] :
             corpus = "Haskins"
-        elif self.name in  ["fsew0", "msak0", "faet0", "ffes0", "maps0", "mjjn0", "falh0"]:
+        elif self.speaker in  ["fsew0", "msak0", "faet0", "ffes0", "maps0", "mjjn0", "falh0"]:
             corpus = "mocha"
         else:
             raise NameError("vous navez pas choisi un des speasker")
-        self.corpus_name =  corpus
+        self.corpus = corpus
 
-    def smooth_data(self,ema,sr = 0):
-        #TODO
+    def smooth_data(self, ema, sr=0):
+        """
+        :param ema: one ema trajectory
+        :param sr: sampling rate of the ema trajectory
+        :return:  the smoothed ema trajectory
+        """
         pad = 30
         if sr == 0:
             sr = self.sampling_rate_ema
@@ -157,15 +161,20 @@ class Speaker():
         my_ema_filtered = my_ema_filtered[pad:-pad, :]
         return my_ema_filtered
 
-
     def calculate_norm_values(self):
-        #TODO
+        """
+        based on all the EMA trajectories and frames MFCC calculate the norm values :
+        - mean of ema and mfcc
+        - std of ema and mfcc
+        - moving average for ema on 60 sentences
+        then save those norm values
+        """
         list_EMA_traj = self.list_EMA_traj
         list_MFCC_frames = self.list_MFCC_frames
 
         pad = 30
         all_mean_ema = np.array([np.mean(traj, axis=0) for traj in list_EMA_traj])
-        np.save(os.path.join("norm_values", "all_mean_ema_" + self.name), all_mean_ema)
+        np.save(os.path.join("norm_values", "all_mean_ema_" + self.speaker), all_mean_ema)
         #    weights_moving_average = low_pass_filter_weight(cut_off=10, sampling_rate=self.sampling_rate_ema)
         all_mean_ema = np.concatenate([np.expand_dims(np.pad(all_mean_ema[:, k], (pad, pad), "symmetric"), 1)
                                        for k in range(all_mean_ema.shape[1])], axis=1)  # rajoute pad avant et apres
@@ -175,18 +184,18 @@ class Speaker():
 
         all_EMA_concat = np.concatenate([traj for traj in list_EMA_traj], axis=0)
         std_ema = np.std(all_EMA_concat, axis=0)
-        std_ema[std_ema < 1e-3]=1
+        std_ema[std_ema < 1e-3] = 1
 
         mean_ema = np.mean(np.array([np.mean(traj, axis=0) for traj in list_EMA_traj]),
-                           axis=0)  # apres que chaque phrase soit centrée
+                           axis=0)
         std_mfcc = np.mean(np.array([np.std(frame, axis=0) for frame in list_MFCC_frames]), axis=0)
         mean_mfcc = np.mean(np.array([np.mean(frame, axis=0) for frame in list_MFCC_frames]), axis=0)
 
-        np.save(os.path.join("norm_values", "moving_average_ema_" + self.name), moving_average)
-        np.save(os.path.join("norm_values", "std_ema_" + self.name), std_ema)
-        np.save(os.path.join("norm_values", "mean_ema_" + self.name), mean_ema)
-        np.save(os.path.join("norm_values", "std_mfcc_" + self.name), std_mfcc)
-        np.save(os.path.join("norm_values", "mean_mfcc_" + self.name), mean_mfcc)
+        np.save(os.path.join("norm_values", "moving_average_ema_" + self.speaker), moving_average)
+        np.save(os.path.join("norm_values", "std_ema_" + self.speaker), std_ema)
+        np.save(os.path.join("norm_values", "mean_ema_" + self.speaker), mean_ema)
+        np.save(os.path.join("norm_values", "std_mfcc_" + self.speaker), std_mfcc)
+        np.save(os.path.join("norm_values", "mean_mfcc_" + self.speaker), mean_mfcc)
 
         self.std_ema = std_ema
         self.moving_average_ema = moving_average
@@ -194,57 +203,74 @@ class Speaker():
         self.mean_mfcc = mean_mfcc
         self.std_mfcc = std_mfcc
 
-    def add_vocal_tract(self,my_ema):
+    def add_vocal_tract(self , my_ema):
         """
-        # TODO: description de la fonction, en ANGLAIS
-        :param my_ema: trajectoires EMA (K points) disponibles
-        :return: tableau (18,K) en rajoutant les 4 vocal tract , mettant à 0 les trajectoires non disponibles,
-        et reorganise dans l'ordre les trajectoires.
+        calculate 4 'vocal tract' and reorganize the data into a 18 trajectories in a precised order
+        :param my_ema: EMA trajectory with K points
+        :return: a np array (18,K) where the trajectories are sorted, and unavailable trajectories are at 0
         """
-        #   print("adding vocal tracts for speaker {}".format(speaker))
         def add_lip_aperture(ema):
-            #TODO
+            """
+            :param ema: 1 ema trajectory
+            :return: return lip aperture trajectory upperlip_y - lowerlip_y
+            """
             ind_1, ind_2 = [self.articulators.index("ul_y"), self.articulators.index("ll_y")]
             lip_aperture = ema[:, ind_1] - ema[:, ind_2]  # upperlip_y - lowerlip_y
             return lip_aperture
 
         def add_lip_protrusion(ema):
-            #TODO
+            """
+            :param ema: 1 ema trajectory
+            :return: return lip protrusion trajectory (upperlip_x + lowerlip_x)/2
+            """
             ind_1, ind_2 = [self.articulators.index("ul_x"), self.articulators.index("ll_x")]
             lip_protrusion = (ema[:, ind_1] + ema[:, ind_2]) / 2
             return lip_protrusion
 
         def add_TTCL(ema):  # tongue tip constriction location in degree
-            #TODO
+            """
+           :param ema: 1 ema trajectory
+           :return: return tongue tip constriction location in degree trajectory .
+           Formula to check again , corresponds to the angle between the horizontal and the tongue tip location
+           """
             ind_1, ind_2 = [self.articulators.index("tt_x"), self.articulators.index("tt_y")]
-            TTCL = ema[:, ind_1] / np.sqrt(ema[:, ind_1] ** 2 + ema[:, ind_2] ** 2)  # upperlip_y - lowerlip_y
+            TTCL = ema[:, ind_1] / np.sqrt(ema[:, ind_1] ** 2 + ema[:, ind_2] ** 2)
             return TTCL
 
-        def add_TBCL(ema):  # tongue body constriction location in degree
-            #TODO
+        def add_TBCL(ema):
+            """
+            :param ema: 1 ema trajectory
+           :return: return tongue body constriction location in degree trajectory .
+           Formula to check again , corresponds to the angle between the horizontal and the tongue body location
+            """
             ind_1, ind_2 = [self.articulators.index("tb_x"), self.articulators.index("tb_y")]
             TBCL = ema[:, ind_1] / np.sqrt(ema[:, ind_1] ** 2 + ema[:, ind_2] ** 2)  # upperlip_y - lowerlip_y
             return TBCL
 
         def get_idx_to_ignore():
-            #TODO
+            """
+            :return: index of articulations that are not available for this speaker. Based on the local csv file
+            """
             arti_per_speaker = os.path.join(root_folder, "Traitement", "articulators_per_speaker.csv")
             csv.register_dialect('myDialect', delimiter=';')
             with open(arti_per_speaker, 'r') as csvFile:
                 reader = csv.reader(csvFile, dialect="myDialect")
                 next(reader)
                 for row in reader:
-                    if row[0] == self.name:
+                    if row[0] == self.speaker:
                         arti_to_consider = row[1:19]
             idx_to_ignore = [k for k, n in enumerate(arti_to_consider) if n == "0"]
             return idx_to_ignore
 
         lip_aperture = add_lip_aperture(my_ema)
+
         lip_protrusion = add_lip_protrusion(my_ema)
+
         TTCL = add_TTCL(my_ema)
+
         TBCL = add_TBCL(my_ema)
 
-        if self.name in self.speakers_with_velum:  # 14 arti de 0 à 13 (2*6 + 2)
+        if self.speaker in self.speakers_with_velum:  # 14 arti de 0 à 13 (2*6 + 2)
             my_ema = np.concatenate((my_ema, np.zeros((len(my_ema), 4))), axis=1)
             my_ema[:, 16:18] = my_ema[:, 12:14]  # met les velum dans les 2 dernieres arti
             my_ema[:, 12:16] = 0  # les 4 autres colonnes vont etre remplies avec les VT par la suite
@@ -261,18 +287,25 @@ class Speaker():
         return my_ema
 
     def normalize_phrase(self,i,my_ema_filtered,my_mfcc):
-        #TODO
+        """
+        :param i: index of the ema traj (to get the moving average)
+        :param my_ema_filtered: the ema smoothed ema traj
+        :param my_mfcc: mfcc frames
+        :return: the normalized EMA et MFCC data
+        """
         my_ema_VT = (my_ema_filtered - self.moving_average_ema[i, :]) / self.std_ema
         my_mfcc = (my_mfcc - self.mean_mfcc) / self.std_mfcc
         return my_ema_VT,my_mfcc
 
     def synchro_ema_mfcc(self,my_ema, my_mfcc):
-        #TODO
+        """
+        :param my_ema: ema traj
+        :param my_mfcc: corresponding mfcc frames
+        :return: ema and mfcc synchronized
+        the ema traj is downsampled to have 1 position for 1 frame mfcc
+        """
+
         my_ema = scipy.signal.resample(my_ema, num=len(my_mfcc))
         return my_ema, my_mfcc
 
 
-# TODO: c'est utile ça ? Si non tu supprimes
-#aa = Speaker("fsew0")
-#aa.smooth_data("yo")
-#print(aa.speakers_with_velum)
