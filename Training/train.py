3de3be701c356752d1139e579758885052a31cc3
Parrot Canudo Maud <maudparrot@hotmail.fr>
2019-09-13 11:15:43 +0200

    temp

--- Training/train.py
+++ Training/train.py
@@ -148,7 +148,7 @@ def train_model(test_on, n_epochs, loss_train, patience, select_arti, corpus_to_
 
     criterion = criterion_both(loss_train, cuda_avail, device)
 
-    criterion = criterion_pearson
+  #  criterion = criterion_pearson
 
     files_per_categ, files_for_test = give_me_train_valid_test_filenames(train_on,test_on,config, batch_size)
 
@@ -187,7 +187,7 @@ def train_model(test_on, n_epochs, loss_train, patience, select_arti, corpus_to_
                    # y_pred[:,:,idx_to_ignore].detach()
                     #y[:,:,idx_to_ignore].requires_grad = False
 
-                loss = criterion(y, y_pred, cuda_avail = cuda_avail, device=device)
+                loss = criterion(y, y_pred)# cuda_avail = cuda_avail, device=device)
                 loss.backward()
                 optimizer.step()
                 torch.cuda.empty_cache()
@@ -219,7 +219,7 @@ def train_model(test_on, n_epochs, loss_train, patience, select_arti, corpus_to_
                     #    y_pred[:, :, idx_to_ignore].detach()
                    #     y[:, :, idx_to_ignore].requires_grad = False
 
-                    loss_courant = criterion(y, y_pred,cuda_avail = cuda_avail, device=device)
+                    loss_courant = criterion(y, y_pred) #,cuda_avail = cuda_avail, device=device)
 
                     loss_vali += loss_courant.item()
             loss_vali  = loss_vali/n_valid
