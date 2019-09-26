27015d23f38eb121361509d2fe298c415db49fc1
Juliette Millet <juliette.milletviolette@wanadoo.fr>
2019-09-26 10:11:22 +0200

    add common option

--- Training/experiment.py
+++ Training/experiment.py
@@ -20,13 +20,15 @@ sys.path.insert(0,parentdir)
 
 
 from Preprocessing.tools_preprocessing import get_speakers_per_corpus
+from Training.tools_learning import give_me_common_articulators
 from Training.train import train_model
+from Training.train_only_common import train_model_arti_common
 import numpy as np
 import argparse
 import csv
 from datetime import date
 
-n_epochs = 50
+n_epochs = 500
 loss_train = 90
 patience = 5
 select_arti = True
@@ -41,7 +43,7 @@ output_dim = 18 #18 trajectories
 speakers = get_speakers_per_corpus("Haskins")
 config = "indep"
 
-def cross_val_config(corpus_to_train_on):
+def cross_val_config(corpus_to_train_on, only_common = False):
     """
     performs the cross validation on Haskins corpus for 3 types of config in order to evaluate the capacity of
     generalization of the dataset. the parameters (other than config) are defined above and can be modified
@@ -50,16 +52,23 @@ def cross_val_config(corpus_to_train_on):
     speakers = []
     for co in str(corpus_to_train_on[1:-1]).split(","):
         speakers = speakers + get_speakers_per_corpus(co)
-
+    if only_common:
+        output_dim = len(give_me_common_articulators(speakers))
     for config in ["spec", "indep", "dep"]:
         count = 0
         rmse_all, pearson_all = np.zeros((len(speakers), output_dim)), np.zeros((len(speakers), output_dim))
         for speaker in speakers :
-
-            rmse, pearson = train_model(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train,patience=patience,
-                                        select_arti=select_arti, corpus_to_train_on=corpus_to_train_on,
-                                        batch_norma=batch_norma, filter_type=filter_type, to_plot=to_plot,
-                                        lr=lr, delta_test=delta_test, config=config)
+            if only_common:
+                rmse, pearson = train_model_arti_common(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train,
+                                            patience=patience,
+                                            corpus_to_train_on=corpus_to_train_on,
+                                            batch_norma=batch_norma, filter_type=filter_type, to_plot=to_plot,
+                                            lr=lr, delta_test=delta_test, config=config)
+            else:
+                rmse, pearson = train_model(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train,patience=patience,
+                                            select_arti=select_arti, corpus_to_train_on=corpus_to_train_on,
+                                            batch_norma=batch_norma, filter_type=filter_type, to_plot=to_plot,
+                                            lr=lr, delta_test=delta_test, config=config)
 
             rmse_all[count, :] = rmse
             pearson_all[count, :] = pearson
@@ -85,7 +94,7 @@ def cross_val_config(corpus_to_train_on):
                 writer.writerow(row)
 
 
-def cross_val_filter(corpus_to_train_on, config):
+def cross_val_filter(corpus_to_train_on, config, only_common = False):
     """
     performs the cross validation on Haskins corpus for 3 types of filter in order to evaluate the impact of the filter
     the parameters (other than typefilter) are defined above and can be modified
@@ -95,15 +104,23 @@ def cross_val_filter(corpus_to_train_on, config):
     speakers = []
     for co in str(corpus_to_train_on[1:-1]).split(","):
         speakers = speakers + get_speakers_per_corpus(co)
-
+    if only_common:
+        output_dim = len(give_me_common_articulators(speakers))
     for filter_type in ["unfix","out","fix"]:
         count = 0
         rmse_all, pearson_all = np.zeros((len(speakers), output_dim)), np.zeros((len(speakers), output_dim))
         for speaker in speakers :
-            rmse, pearson = train_model(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train,patience=patience,
-                                        select_arti=select_arti, corpus_to_train_on=corpus_to_train_on,
-                                        batch_norma=batch_norma, filter_type=filter_type, to_plot=to_plot,
-                                        lr=lr, delta_test=delta_test, config=config)
+            if only_common:
+                rmse, pearson = train_model_arti_common(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train,
+                                            patience=patience,
+                                            corpus_to_train_on=corpus_to_train_on,
+                                            batch_norma=batch_norma, filter_type=filter_type, to_plot=to_plot,
+                                            lr=lr, delta_test=delta_test, config=config)
+            else:
+                rmse, pearson = train_model(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train,patience=patience,
+                                            select_arti=select_arti, corpus_to_train_on=corpus_to_train_on,
+                                            batch_norma=batch_norma, filter_type=filter_type, to_plot=to_plot,
+                                            lr=lr, delta_test=delta_test, config=config)
             rmse_all[count, :] = rmse
             pearson_all[count, :] = pearson
             count += 1
@@ -171,7 +188,7 @@ def cross_val_batch_norma(corpus_to_train_on, config):
                 writer.writerow(row)
 
 
-def cross_val_for_alpha(corpus_to_train_on,config):
+def cross_val_for_alpha(corpus_to_train_on,config, only_common = False):
     """
         performs the cross validation on corpus_to_train_on corpus for different values of alpha in the combined loss
         experiment to determine the optimal alpha
@@ -185,17 +202,30 @@ def cross_val_for_alpha(corpus_to_train_on,config):
     # TODO: delete that it is just to make a small test
     speakers = ["fsew0", "msak0", "MNGU0"]
 
+    if only_common:
+        output_dim = len(give_me_common_articulators(speakers))
+
     loss_range = [0, 20, 40, 60, 80, 100]
 
     for loss_train in loss_range:
         count = 0
         rmse_all, pearson_all = np.zeros((len(speakers), output_dim)), np.zeros((len(speakers), output_dim))
+
         for speaker in speakers:
             speaker_to_train = str([sp for sp in speakers if sp != speaker])
-            rmse, pearson = train_model(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train, patience=patience,
-                                        select_arti=select_arti, corpus_to_train_on=corpus_to_train_on,
-                                        batch_norma=batch_norma, filter_type=filter_type, to_plot=to_plot,
-                                        lr=lr, delta_test=delta_test, config=config, speakers_to_train_on=speaker_to_train)
+
+            if only_common:
+                rmse, pearson = train_model_arti_common(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train, patience=patience,
+                                            corpus_to_train_on=corpus_to_train_on,
+                                            batch_norma=batch_norma, filter_type=filter_type, to_plot=to_plot,
+                                            lr=lr, delta_test=delta_test, config=config, speakers_to_train_on=speaker_to_train)
+            else:
+                rmse, pearson = train_model(test_on=speaker, n_epochs=n_epochs, loss_train=loss_train,
+                                            patience=patience,
+                                            select_arti=select_arti, corpus_to_train_on=corpus_to_train_on,
+                                            batch_norma=batch_norma, filter_type=filter_type, to_plot=to_plot,
+                                            lr=lr, delta_test=delta_test, config=config,
+                                            speakers_to_train_on=speaker_to_train)
             rmse_all[count, :] = rmse
             pearson_all[count, :] = pearson
             count += 1
@@ -230,23 +260,24 @@ if __name__=='__main__':
 
     parser.add_argument('--config', type=str, default = None,
                         help='indep, spec or dep')
+    parser.add_argument('--only_common_arti', type=bool, default = False, help='True or False' )
 
     args = parser.parse_args()
 
     if args.experiment_type == "config":
-        cross_val_config(args.corpus_exp)
+        cross_val_config(args.corpus_exp, only_common=args.only_common_arti)
 
     elif args.experiment_type == "filter":
         if args.config is None :
             print("you have to precise the config for this experiment")
         else:
-            cross_val_filter(args.corpus_exp, args.config)
+            cross_val_filter(args.corpus_exp, args.config, only_common=args.only_common_arti)
 
     elif args.experiment_type == "alpha":
         if args.config is None:
             print("you have to precise the config for this experiment")
         else:
-            cross_val_for_alpha(args.corpus_exp,args.config)
+            cross_val_for_alpha(args.corpus_exp,args.config, only_common=args.only_common_arti)
 
     elif args.experiment_type == "bn":
         if args.config is None:
