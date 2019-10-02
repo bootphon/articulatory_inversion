c7494d82d66f8825cbd3cbd65eb4b6dc17b37fa8
Juliette Millet <juliette.milletviolette@wanadoo.fr>
2019-10-02 08:12:05 +0200

    alpha exp

--- Training/experiment.py
+++ Training/experiment.py
@@ -249,7 +249,10 @@ def cross_val_for_alpha(corpus_to_train_on,config, only_common = False):
         print("PEARSON ", results_pearson)
         print(" PEARSON STD", std_pearson)
         today = date.today().strftime("%d/%m/%Y")
-        with open('experiment_results_alpha_haskins.csv', 'a') as f:
+        name = 'experiment_results_alpha_' + '_'.join(speakers) + '.csv'
+        f = open(name, 'w')
+        f.close()
+        with open(name, 'a') as f:
             writer = csv.writer(f)
             row_rmse_mean = [today, corpus_to_train_on, loss_train, "rmse_mean"] + results_rmse.tolist()
             row_rmse_std = [today,corpus_to_train_on, loss_train, "rmse_std"] + std_rmse.tolist()
